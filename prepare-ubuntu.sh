f1ab1fbb54003c532877195960f6ff903941a352
N.J.L. Boonstra <nielsboonstra@xs4all.nl>
2020-05-31 13:19:17 +0200

    added some more installs for better automation

--- prepare-ubuntu.sh
+++ prepare-ubuntu.sh
@@ -4,4 +4,10 @@ sudo apt update
 sudo apt install qemu-kvm python3.7 python3.7-venv -y
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 sudo python3.7 ./get-pip.py
+sudo python3.7 -m pip install pipenv
+python3.7 -m pipenv install
 sudo usermod -aG kvm $USER
+
+mkdir ~/bin
+curl -fsSL https://github.com/firecracker-microvm/firecracker/releases/download/v0.21.1/firecracker-v0.21.1-$(uname -m) -o ~/bin/firecracker
+chmod +x ~/bin/firecracker
