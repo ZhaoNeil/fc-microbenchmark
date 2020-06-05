8e95dead6a2b8ae5912a54e54075fecd11c08736
N.J.L. Boonstra <nielsboonstra@xs4all.nl>
2020-06-05 13:02:02 +0200

    Added additional packages needed for cpupower

--- prepare-ubuntu.sh
+++ prepare-ubuntu.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo apt update
-sudo apt install qemu-kvm python3.7 python3.7-venv linux-tools-common -y
+sudo apt install qemu-kvm python3.7 python3.7-venv linux-tools-common linux-cloud-tools-$(uname -r) linux-tools-$(uname -r) -y
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 sudo python3.7 ./get-pip.py
 sudo python3.7 -m pip install pipenv
